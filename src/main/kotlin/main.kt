8667b5683688ec408772431928efb7cf3dda4695
David Lantos <david.lantos@gmail.com>
2020-06-10 11:45:53 +0100

    Be explicit about which workspace ID we use

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -9,16 +9,16 @@ import com.structurizr.util.WorkspaceUtils
 import uk.gov.justice.hmpps.architecture.custody.*
 
 object App {
+  const val CUSTODY_WORKSPACE_ID: Long = 55246
+
   @JvmStatic
   fun main(@Suppress("UNUSED_PARAMETER") args: Array<String>) {
-    val workspaceId: Long = 55246
-
     val client = StructurizrClient(System.getenv("STRUCTURIZR_API_KEY"), System.getenv("STRUCTURIZR_API_SECRET"))
     val workspace = Workspace("Custody systems", "Systems related to the confinement of offenders")
 
     CustodyModel(workspace.model)
     CustodyViews(workspace.model, workspace.views)
 
-    client.putWorkspace(workspaceId, workspace)
+    client.putWorkspace(CUSTODY_WORKSPACE_ID, workspace)
   }
 }
