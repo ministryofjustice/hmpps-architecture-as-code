6e8da2b574b031848ca8725a575b411a4481f28a
Phil Roffe <phil.roffe@digital.justice.gov.uk>
2020-06-22 22:22:18 +0100

    Added Custody API, NOMIS API (Deprecated) and Offender Events containers.  These services all require direct NOMIS DB connectivity

--- src/main/kotlin/prison/nomis.kt
+++ src/main/kotlin/prison/nomis.kt
@@ -18,6 +18,22 @@ class NOMIS(model: Model) {
 
     nomis.addContainer("Elite2 API", "API over the NOMIS DB used by Digital Prison team applications and services", null).apply {
       setUrl("https://github.com/ministryofjustice/elite2-api")
+      addTags("database")
+      uses(db, "JDBC")
+    }
+
+    nomis.addContainer("Custody API", "New Tech Nomis: Offender API.  The service provides REST access to the Nomis Oracle DB offender information.", null).apply {
+      setUrl("https://github.com/ministryofjustice/custody-api")
+      uses(db, "JDBC")
+    }
+
+    nomis.addContainer("NOMIS API (Deprecated)", "(Deprecated) REST API for NOMIS which connects to Oracle DB. Deprecated - please use Custody API instead.", null).apply {
+      setUrl("https://github.com/ministryofjustice/nomis-api")
+      uses(db, "JDBC")
+    }
+
+    nomis.addContainer("Offender Events", "Publishes Events about offender change to Pub / Sub Topics.", null).apply {
+      setUrl("https://github.com/ministryofjustice/offender-events")
       uses(db, "JDBC")
     }
 
