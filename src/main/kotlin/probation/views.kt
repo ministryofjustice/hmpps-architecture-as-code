9f7ced6884abd37131dbe084283737dda5cf2a40
David Lantos <david.lantos@gmail.com>
2020-06-24 22:28:32 +0100

    Add dashes to probation view keys
    They make it a lot more legible, which is always a plus

--- src/main/kotlin/probation/views.kt
+++ src/main/kotlin/probation/views.kt
@@ -12,14 +12,14 @@ fun probationViews(model: Model, views: ViewSet) {
   }
 
   val epf = model.getSoftwareSystemWithName("EPF")!!
-  views.createSystemContextView(epf, "epfcontext", null).apply {
+  views.createSystemContextView(epf, "epf-context", null).apply {
     addDefaultElements()
     addNearestNeighbours(model.getPersonWithName("EPF Product Manager")!!)
     enableAutomaticLayout()
   }
 
   val im = model.getSoftwareSystemWithName("IM")!!
-  views.createSystemContextView(im, "interventionsmanagercontext", null).apply {
+  views.createSystemContextView(im, "interventions-manager-context", null).apply {
     addDefaultElements()
     enableAutomaticLayout()
   }
@@ -27,20 +27,20 @@ fun probationViews(model: Model, views: ViewSet) {
   addDelius(model.getSoftwareSystemWithName("nDelius")!!, views)
 
   val ndmis = model.getSoftwareSystemWithName("NDMIS")!!
-  views.createSystemContextView(ndmis, "ndmiscontext", null).apply {
+  views.createSystemContextView(ndmis, "ndmis-context", null).apply {
     addDefaultElements()
     enableAutomaticLayout(AutomaticLayout.RankDirection.LeftRight, 200, 200)
   }
 }
 
 private fun addDelius(delius: SoftwareSystem, views: ViewSet) {
-  views.createSystemContextView(delius, "deliusdatacontext", null).apply {
+  views.createSystemContextView(delius, "delius-data-context", null).apply {
     addDefaultElements()
     model.people.forEach(this::remove)
     enableAutomaticLayout()
   }
 
-  views.createSystemContextView(delius, "deliuspeoplecontext", null).apply {
+  views.createSystemContextView(delius, "delius-people-context", null).apply {
     addDefaultElements()
     model.softwareSystems.filter { it != delius }.forEach(this::remove)
     enableAutomaticLayout()
