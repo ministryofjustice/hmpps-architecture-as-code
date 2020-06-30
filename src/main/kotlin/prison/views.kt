3d2aa39667340f64fe87875dc85a0a1add6dac4b
Ryan Brooks <ryan@slatehorse.com>
2020-06-30 16:55:25 +0100

    removed styles (we'll use the probation styles to dry it up)

--- src/main/kotlin/prison/views.kt
+++ src/main/kotlin/prison/views.kt
@@ -10,15 +10,6 @@ const val DATABASE_TAG = "database";
 
 fun prisonViews(model: Model, views: ViewSet) {
   
-
-  /**
-   * Add custom styles to differentiate containers, e.g. Person, Database
-   */
-  val styles = views.getConfiguration().getStyles();
-  //styles.addElementStyle(Tags.SOFTWARE_SYSTEM).background("#1168bd").color("#ffffff");
-  styles.addElementStyle(Tags.PERSON).background("#08427b").color("#ffffff").shape(Shape.Person);
-  styles.addElementStyle(DATABASE_TAG).shape(Shape.Cylinder);
-  
   views.createSystemLandscapeView("everything", "Absolutely everything").apply {
     addDefaultElements()
     enableAutomaticLayout()
