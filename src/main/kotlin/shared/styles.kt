f047fce4ac9eea2298db115ac1b7b19dac374323
Ryan Brooks <ryan@slatehorse.com>
2020-07-01 10:05:39 +0100

    refactor additional prison style into shared styles

--- src/main/kotlin/shared/styles.kt
+++ src/main/kotlin/shared/styles.kt
@@ -9,6 +9,8 @@ fun styles(styles: Styles) {
   styles.addElementStyle("database").shape(Shape.Cylinder)
   styles.addElementStyle("Person").shape(Shape.Person).background("#0099cc")
 
+  styles.addElementStyle("WebBrowser").shape(Shape.WebBrowser)
+
   styles.addElementStyle("external").color("#000000").background("#ccff99")
   styles.addElementStyle("deprecated").background("#999999")
 }
