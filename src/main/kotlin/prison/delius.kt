409f7acd110d206eab68844d46462087350b7223
Gareth Davies <gareth.davies@digital.justice.gov.uk>
2020-07-01 10:25:14 +0100

    Align model structure with other prison models

--- src/main/kotlin/prison/delius.kt
+++ src/main/kotlin/prison/delius.kt
@@ -1,38 +1,50 @@
 package uk.gov.justice.hmpps.architecture.prison
 
-import com.structurizr.model.Model
 import com.structurizr.model.Container
+import com.structurizr.model.Model
 import com.structurizr.model.SoftwareSystem
 
 class DELIUS(model: Model) {
   val system: SoftwareSystem
   val db: Container
 
+  companion object {
+    const val DATABASE_TAG = "database";
+    const val SOFTWARE_AS_A_SERVICE_TAG = "SAAS";
+  }
+
+  /**
+   *
+   */
+
   init {
-    val delius = model.addSoftwareSystem("nDelius",
+
+    system = model.addSoftwareSystem("nDelius",
         "National Delius\nSupporting the management of offenders and delivering national reporting and performance monitoring data")
 
-    db = delius.addContainer("nDelius database", null, "Oracle").apply {
+    db = system.addContainer("nDelius database", null, "Oracle").apply {
       addTags(DATABASE_TAG)
+      addTags(SOFTWARE_AS_A_SERVICE_TAG)
     }
 
-    delius.addContainer("CommunityAPI",
+    val elasticSearchStore = system.addContainer("ElasticSearch store",
+        "Data store for feedback collection, and indexing for Drupal CMS content", "ElasticSearch")
+        .apply {
+          addTags(DATABASE_TAG)
+          addTags(SOFTWARE_AS_A_SERVICE_TAG)
+        }
+
+    system.addContainer("CommunityAPI",
         "API over the nDelius DB used by HMPPS Digital team applications and services", "Java")
         .apply {
           setUrl("https://github.com/ministryofjustice/community-api")
           uses(db, "JDBC")
         }
 
-    val elasticSearch = delius.addContainer("ElasticSearch", "Elasticsearch index of nDelius data",
-        null).apply {
-      addTags("External")
-    }
-
-    delius.addContainer("OffenderSearch",
+    system.addContainer("OffenderSearch",
         "API over the nDelius offender data held in Elasticsearch", "Java").apply {
-      uses(elasticSearch, "Queries offender data from nDelius Elasticsearch Index")
+      uses(elasticSearchStore, "Queries offender data from nDelius Elasticsearch Index")
     }
 
-    system = delius
   }
 }
