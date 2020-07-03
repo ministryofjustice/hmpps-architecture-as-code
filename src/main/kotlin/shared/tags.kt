05e9bdee839703e863ddca493ceee122ca1f5473
David Lantos <david.lantos@gmail.com>
2020-07-03 12:36:18 +0100

    Clarify that tags can be added to any model element

--- src/main/kotlin/shared/tags.kt
+++ src/main/kotlin/shared/tags.kt
@@ -6,7 +6,7 @@ import com.structurizr.model.Relationship
 enum class Tags() {
   DATABASE, WEB_BROWSER, EXTERNAL, PROVIDER, DEPRECATED, SOFTWARE_AS_A_SERVICE, PRISON_SERVICE;
 
-  // Usage example: Tags.DATABASE.addTo(container)
+  // Usage example: Tags.DATABASE.addTo(any_model_element)
   fun addTo(element: Element) {
     element.addTags(this.toString())
   }
