25af9feaba5add48d8302fd1d45b87f7a9ebb323
David Lantos <david.lantos@gmail.com>
2020-07-03 12:38:16 +0100

    Remove unused "external" tag
    I realised it's not used elsewhere

--- src/main/kotlin/shared/tags.kt
+++ src/main/kotlin/shared/tags.kt
@@ -4,7 +4,7 @@ import com.structurizr.model.Element
 import com.structurizr.model.Relationship
 
 enum class Tags() {
-  DATABASE, WEB_BROWSER, EXTERNAL, PROVIDER, DEPRECATED, SOFTWARE_AS_A_SERVICE, PRISON_SERVICE;
+  DATABASE, WEB_BROWSER, PROVIDER, DEPRECATED, SOFTWARE_AS_A_SERVICE, PRISON_SERVICE;
 
   // Usage example: Tags.DATABASE.addTo(any_model_element)
   fun addTo(element: Element) {
