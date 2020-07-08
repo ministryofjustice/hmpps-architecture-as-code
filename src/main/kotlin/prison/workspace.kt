8115cf8aa15c132bf4bdbb3318be2b146580b28d
Ryan Brooks <ryan@slatehorse.com>
2020-07-08 09:11:40 +0100

    Add HMPPS Prisons enterprise for bounded box in System Context and Systems Landscape diagrams

--- src/main/kotlin/prison/workspace.kt
+++ src/main/kotlin/prison/workspace.kt
@@ -1,11 +1,15 @@
 package uk.gov.justice.hmpps.architecture.prison
 
+import com.structurizr.model.Enterprise
 import com.structurizr.Workspace
 import uk.gov.justice.hmpps.architecture.shared.cloudPlatform
 import uk.gov.justice.hmpps.architecture.shared.styles
 
 fun prisonWorkspace(): Workspace {
-  val workspace = Workspace("Prison systems", "Systems related to the confinement of offenders")
+  val hmpps_prisons = Enterprise("HMPPS Prisons")
+  val workspace = Workspace("Prison systems", "Systems related to the confinement of offenders").apply {
+    model.setEnterprise(hmpps_prisons)
+  }
   workspace.id = 55246
 
   cloudPlatform(workspace.model)
