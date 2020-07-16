b9f9eb5d10eaea71a7c1fb9df081237a8b2ed8be
Ryan Brooks <ryan.brooks@digital.justice.gov.uk>
2020-07-16 10:33:01 +0100

    Add comment about not defining models in models.kt, for https://github.com/ministryofjustice/hmpps-architecture-as-code/pull/15/files\#r455641185

--- src/main/kotlin/prison/model.kt
+++ src/main/kotlin/prison/model.kt
@@ -5,14 +5,18 @@ import com.structurizr.model.Model
 
 import uk.gov.justice.hmpps.architecture.probation.Delius
 
+/**
+  * Please don't define models here. New models should be added
+  * to workspace.kt following the patterns there.
+  *
+  * TODO: refactor these models to use the new approach, and remove this file
+  */
 fun prisonModel(model: Model) {
   model.setImpliedRelationshipsStrategy(
-      CreateImpliedRelationshipsUnlessAnyRelationshipExistsStrategy())
+    CreateImpliedRelationshipsUnlessAnyRelationshipExistsStrategy())
 
   HmmpsAuth(model)
   Delius(model)
   NDH(model).system
   PATHFINDER(model)
-
-  
 }
