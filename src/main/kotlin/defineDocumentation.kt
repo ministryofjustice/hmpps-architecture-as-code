e43ef7976db9bf5dfa9cf942ba87cfda4672bcb4
David Lantos <david.lantos@gmail.com>
2020-08-19 10:35:17 +0100

    Publish API documentation to Structurizr
    The first page is a page that is auto-generated from `APIDocs`
    annotations on containers.

---
+++ src/main/kotlin/defineDocumentation.kt
@@ -0,0 +1,49 @@
+package uk.gov.justice.hmpps.architecture
+
+import com.structurizr.Workspace
+import com.structurizr.documentation.AutomaticDocumentationTemplate
+import com.structurizr.model.Container
+import com.structurizr.model.Model
+import java.io.File
+import java.io.PrintWriter
+
+fun defineDocumentation(workspace: Workspace) {
+  val docsRoot = File(App.EXPORT_LOCATION, "docs/")
+  docsRoot.mkdirs()
+
+  val w = File(docsRoot, "apidocs.md").printWriter()
+  writeAPIs(w, workspace.model)
+  w.close()
+
+  val template = AutomaticDocumentationTemplate(workspace)
+  template.addSections(docsRoot)
+}
+
+private fun writeAPIs(w: PrintWriter, model: Model) {
+  w.println("## Published APIs")
+  w.println("")
+
+  val apis = model.softwareSystems
+    .sortedBy { it.name.toLowerCase() }
+    .flatMap { it.containers }
+    .filter { it.properties.get("api-docs-url") != null }
+  w.println("| Software System | API name | Links |")
+  w.println("| --- | --- | --- |")
+  apis.forEach { writeAPI(w, it) }
+}
+
+private fun writeAPI(w: PrintWriter, apiContainer: Container) {
+  w.print("| ")
+  w.print(apiContainer.softwareSystem.name)
+
+  w.print("| ")
+  w.print(apiContainer.name)
+
+  w.print("| ")
+  val githubLink = apiContainer.url
+  val apidocsLink = apiContainer.properties.get("api-docs-url")
+  w.print(" [GitHub](%s)".format(githubLink))
+  w.print(" [APIdocs](%s)".format(apidocsLink))
+
+  w.println("|")
+}
