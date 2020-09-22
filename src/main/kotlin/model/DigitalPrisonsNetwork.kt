09c18e2ef1765a7f71f4c24dd214750ed22ea4d1
Ryan Brooks <ryan@slatehorse.com>
2020-09-22 09:56:31 +0100

    Rename auth to Prisoner Identity Service

--- src/main/kotlin/model/DigitalPrisonsNetwork.kt
+++ src/main/kotlin/model/DigitalPrisonsNetwork.kt
@@ -10,13 +10,13 @@ import uk.gov.justice.hmpps.architecture.annotations.Tags
 class DigitalPrisonsNetwork private constructor() {
   companion object : HMPPSSoftwareSystem {
     lateinit var system: SoftwareSystem
-    lateinit var auth: Container
+    lateinit var identity: Container
 
     override fun defineModelEntities(model: Model) {
       system = model.addSoftwareSystem("Digital Prisons Network", "Provides in-cell devices and access to digital services to prisoners")
 
-      auth = system.addContainer(
-        "Azure AD",
+      identity = system.addContainer(
+        "Prisoner Identity Service",
         "Azure Active Directory, supporting OAuth2 applications and network/device login",
         "Active Directory"
       )
