1efc847677788d48df04e9ebeefc06f3210b145b
David Lantos <david.lantos@gmail.com>
2020-11-05 10:20:19 +0000

    Remove unused whitespace and imports (everywhere)

--- src/main/kotlin/model/PrisonerContentHub.kt
+++ src/main/kotlin/model/PrisonerContentHub.kt
@@ -108,7 +108,7 @@ class PrisonerContentHub private constructor() {
     override fun defineRelationships() {
       contentHubFrontend.uses(NOMIS.prisonApi, "lookup visits, canteen, etc.")
       contentHubFrontend.uses(DigitalPrisonsNetwork.identity, "Prisoner logs in with OAuth2 flow")
-      
+
       frontendProxy.uses(NationalPrisonRadio.system, "proxy OGG livestream audio")
     }
 
