f2cad0a88bb89d63973ccac79f5a0f00a5f410d8
David Lantos <david.lantos@gmail.com>
2021-01-07 20:12:52 +0000

    Retrieve API documentation links from repo READMEs
    There is a pattern emerging in HMPPS. Some applications now have their
    APIs documented via badges in their README:
    
    ```
    [![API docs](https://img.shields.io/badge/API_docs_-view-85EA2D.svg?logo=swagger)](https://community-api-public.test.delius.probation.hmpps.dsd.io/swagger-ui/index.html)
    ```
    source: https://raw.githubusercontent.com/ministryofjustice/community-api/fcdf36f4314d9b6884ce712d6e012f5ab98d2faf/README.md
    
    This change pulls down the repositories and parses the README for the
    badge's link to populate the API list.
    
    (The API list gets generated during `./gradlew run` to
    `exports/docs/apidocs.md` and published later to
    https://structurizr.com/share/56937/documentation#%2F:Published%20APIs)

--- src/main/kotlin/defineDocumentation.kt
+++ src/main/kotlin/defineDocumentation.kt
@@ -4,7 +4,10 @@ import com.structurizr.Workspace
 import com.structurizr.documentation.AutomaticDocumentationTemplate
 import com.structurizr.model.Container
 import com.structurizr.model.Model
+import org.eclipse.jgit.api.errors.GitAPIException
+import uk.gov.justice.hmpps.architecture.annotations.APIDocs
 import java.io.File
+import java.io.IOException
 import java.io.PrintWriter
 
 fun defineDocumentation(workspace: Workspace) {
@@ -23,15 +26,15 @@ private fun writeAPIs(w: PrintWriter, model: Model) {
   w.println("## Published APIs")
   w.println("")
 
-  val apis = model.softwareSystems
-    .sortedBy { it.name.toLowerCase() }
-    .flatMap { it.containers }
-    .filter { it.properties.get("api-docs-url") != null }
   w.println("| Software System | API name | Purpose | Links |")
   w.println("| --- | --- | --- | --- |")
-  apis.forEach { writeAPI(w, it) }
+
+  containersWithAPIDocs(model)
+    .also { println("[defineDocumentation] Found ${it.size} APIs with documentation URLs") }
+    .forEach { writeAPI(w, it) }
 }
 
+@Suppress("DEPRECATION")
 private fun writeAPI(w: PrintWriter, apiContainer: Container) {
   w.print("| ")
   w.print(apiContainer.softwareSystem.name)
@@ -44,9 +47,49 @@ private fun writeAPI(w: PrintWriter, apiContainer: Container) {
 
   w.print("| ")
   val githubLink = apiContainer.url
-  val apidocsLink = apiContainer.properties.get("api-docs-url")
+  val apidocsLink = APIDocs.getFrom(apiContainer)
   w.print(" [GitHub](%s)".format(githubLink))
   w.print(" [APIdocs](%s)".format(apidocsLink))
 
   w.println("|")
+}
+
+@Suppress("DEPRECATION")
+private fun containersWithAPIDocs(model: Model): List<Container> {
+  val containers = model.softwareSystems
+    .sortedBy { it.name.toLowerCase() }
+    .flatMap { it.containers }
+
+  containers
+    .filter { it.url.orEmpty().contains("github.com/ministryofjustice") }
+    .forEach {
+      val oldUrl = APIDocs.getFrom(it)
+      val url = readAPIDocsURLFromRepoReadmeBadge(it)
+
+      if (url != null) {
+        if (oldUrl != null) {
+          println("[defineDocumentation] overriding API docs URL\n - from ${oldUrl}\n - to   $url")
+        }
+        APIDocs(url).addTo(it)
+      }
+    }
+
+  return containers.filter { APIDocs.getFrom(it) != null }
+}
+
+val BADGE_URL_PATTERN = Regex("""\[!\[API docs]\(.*\)]\((.*)\)""")
+private fun readAPIDocsURLFromRepoReadmeBadge(app: Container): String? {
+  var readmeContents = ""
+  try {
+    println()
+    val repoDir = cloneRepository(app.url, app)
+    readmeContents = repoDir.resolve("README.md").readText()
+  } catch (e: GitAPIException) {
+    println("[defineDocumentation] ignoring: $e")
+  } catch (e: IOException) {
+    println("[defineDocumentation] ignoring: $e")
+  }
+
+  val m = BADGE_URL_PATTERN.find(readmeContents)
+  return m?.groups?.get(1)?.value
 }
--- src/main/kotlin/pullADRs.kt
+++ src/main/kotlin/pullADRs.kt
@@ -2,10 +2,7 @@ package uk.gov.justice.hmpps.architecture
 
 import com.structurizr.Workspace
 import com.structurizr.documentation.AdrToolsImporter
-import com.structurizr.model.SoftwareSystem
-import org.eclipse.jgit.api.Git
 import uk.gov.justice.hmpps.architecture.annotations.ADRSource
-import java.io.File
 
 fun pullADRs(workspace: Workspace) {
   val systemsWithADRs = workspace.model.softwareSystems.stream().filter { ADRSource.getFrom(it) != null }
@@ -14,26 +11,10 @@ fun pullADRs(workspace: Workspace) {
     println()
     println("[pullADRs] For ${it.name}")
 
-    val cloneDir = cloneADRRepo(it)
+    val cloneDir = cloneRepository(ADRSource.getFrom(it)!!, it)
     val decisions = AdrToolsImporter(workspace, cloneDir.resolve("doc/adr"))
       .importArchitectureDecisionRecords(it)
 
     println("[pullADRs] Imported ${decisions.size} decisions")
   }
-}
-
-private fun cloneADRRepo(system: SoftwareSystem): File {
-  val repo = ADRSource.getFrom(system)
-
-  val cloneDir = App.EXPORT_LOCATION.resolve("clones").resolve(system.id)
-  cloneDir.deleteRecursively()
-  cloneDir.mkdirs()
-
-  println("[pullADRs] Cloning ADR repo ($repo) to directory '$cloneDir'")
-  Git.cloneRepository()
-    .setURI(repo)
-    .setDirectory(cloneDir)
-    .call()
-
-  return cloneDir
 }
