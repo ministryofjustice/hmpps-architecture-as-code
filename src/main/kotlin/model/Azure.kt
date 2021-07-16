eb6282e5cccae94edf0f9e61f62562be88f6a761
Matthew Ryall <matthew.ryall@digital.justice.gov.uk>
2021-07-16 09:12:57 +0100

    Add Curious Azure account and DPS Curious consumer

--- src/main/kotlin/model/Azure.kt
+++ src/main/kotlin/model/Azure.kt
@@ -6,9 +6,13 @@ import com.structurizr.model.Model
 class Azure private constructor() {
   companion object {
     lateinit var root: DeploymentNode
+    lateinit var nomsdigitech: DeploymentNode
+    lateinit var sequation: DeploymentNode
 
     fun defineDeploymentNodes(model: Model) {
       root = model.addDeploymentNode("Microsoft Azure", "Azure platform", "Azure")
+      nomsdigitech = root.addDeploymentNode("NOMS Digital Studio")
+      sequation = root.addDeploymentNode("Sequation")
     }
   }
 }
