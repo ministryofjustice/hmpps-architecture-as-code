4db07ba5418f32f28bd3a518e71b81e2ff3d1261
Matthew Ryall <matthew.ryall@digital.justice.gov.uk>
2021-10-26 12:15:30 +0100

    Add extra Delius components

--- src/main/kotlin/uk/gov/justice/hmpps/architecture/model/Delius.kt
+++ src/main/kotlin/uk/gov/justice/hmpps/architecture/model/Delius.kt
@@ -4,7 +4,6 @@ import com.structurizr.model.Container
 import com.structurizr.model.Model
 import com.structurizr.model.Person
 import com.structurizr.model.SoftwareSystem
-import com.structurizr.view.AutomaticLayout
 import com.structurizr.view.ViewSet
 import uk.gov.justice.hmpps.architecture.HMPPSSoftwareSystem
 import uk.gov.justice.hmpps.architecture.annotations.ProblemArea
@@ -17,6 +16,7 @@ class Delius private constructor() {
     lateinit var communityApi: Container
     lateinit var offenderSearch: Container
     lateinit var offenderSearchIndexer: Container
+    lateinit var offenderElasticsearchStore: Container
     lateinit var supportTeam: Person
     lateinit var deliusApi: Container
     lateinit var probationOffenderEvents: Container
@@ -26,44 +26,36 @@ class Delius private constructor() {
       val deliusAWSAccount = AWS.london.addDeploymentNode("Delius account")
       val ec2 = deliusAWSAccount.addDeploymentNode("EC2", "AWS Elastic Compute Cloud", "AWS")
       val ecs = deliusAWSAccount.addDeploymentNode("ECS", "AWS Elastic Container Service", "AWS")
+      val batch = deliusAWSAccount.addDeploymentNode("Batch", "AWS Batch", "AWS")
 
-      system = model.addSoftwareSystem(
-        "nDelius",
-        "(National Delius) Supporting the management of offenders and delivering national reporting and performance monitoring data"
-      ).apply {
-        ProblemArea.GETTING_THE_RIGHT_REHABILITATION.addTo(this)
-      }
-
+      // People
       supportTeam = model.addPerson(
         "NDST",
         "(National Delius Support Team) Team supporting changes to data in National Delius"
       )
 
-      database = system.addContainer("nDelius database", null, "Oracle").apply {
+      // Top level NDelius system
+      system = model.addSoftwareSystem(
+        "NDelius",
+        "(National Delius) Supporting the management of offenders and delivering national reporting and performance monitoring data"
+      ).apply {
+        ProblemArea.GETTING_THE_RIGHT_REHABILITATION.addTo(this)
+      }
+
+      // NDelius database components
+      database = system.addContainer(
+        "NDelius database",
+        "NPS supervision and case management data",
+        "Oracle"
+      ).apply {
         Tags.DATABASE.addTo(this)
         ec2.add(this)
       }
 
-      system.addContainer(
-        "nDelius application",
-        "Application logic for Delius", "Java"
-      ).apply {
-        url = "https://github.com/ministryofjustice/delius"
-        uses(database, "connects to", "JDBC")
-        ec2.add(this)
-      }
-
-      val elasticSearchStore = system.addContainer(
-        "ElasticSearch store",
-        "Data store for Delius content", "ElasticSearch"
-      ).apply {
-        Tags.DATABASE.addTo(this)
-        CloudPlatform.elasticsearch.add(this)
-      }
-
+      // API services
       deliusApi = system.addContainer(
         "Delius API",
-        "API over the nDelius DB used by HMPPS Digital team applications and services", "Kotlin"
+        "Write API over the NDelius DB used by HMPPS Digital team applications and services", "Kotlin"
       ).apply {
         url = "https://github.com/ministryofjustice/hmpps-delius-api"
         uses(database, "connects to", "JDBC")
@@ -72,14 +64,101 @@ class Delius private constructor() {
 
       communityApi = system.addContainer(
         "Community API",
-        "API over the nDelius DB used by HMPPS Digital team applications and services", "Java"
+        "API over the NDelius DB used by HMPPS Digital team applications and services", "Java"
       ).apply {
         url = "https://github.com/ministryofjustice/community-api"
         uses(database, "connects to", "JDBC")
-        uses(deliusApi, "Writes data to nDelius using")
+        uses(deliusApi, "Writes data to NDelius using")
         ecs.add(this)
       }
 
+      // Delius application components
+      val documentStore = system.addContainer(
+        "NDelius Document Store",
+        "Storage of documents relating to NPS supervision and case management",
+        "Alfresco"
+      ).apply {
+        ec2.add(this)
+      }
+
+      val userDirectory = system.addContainer(
+        "NDelius User Directory",
+        "Store of NDelius users and roles and provider of authentication services",
+        "OpenLDAP"
+      ).apply {
+        ec2.add(this)
+      }
+
+      val ndelius = system.addContainer(
+        "NDelius application",
+        "Application logic for NDelius",
+        "Java"
+      ).apply {
+        url = "https://github.com/ministryofjustice/delius"
+        uses(database, "connects to", "JDBC")
+        uses(documentStore, "stores and retrieves documents", "HTTPS")
+        uses(userDirectory, "authenticates using", "LDAP")
+        ecs.add(this)
+      }
+
+      val userManagement = system.addContainer(
+        "NDelius User Management Tool",
+        "NDelius user and role management",
+        "Java"
+      ).apply {
+        uses(database, "connects to", "JDBC")
+        uses(userDirectory, "authenticates using", "LDAP")
+        ecs.add(this)
+      }
+
+      system.addContainer(
+        "NextCloud",
+        "NDelius shared file storage for accessing exported data etc.",
+        "PHP Application"
+      ).apply {
+        uses(userDirectory, "authenticates using", "LDAP")
+        ecs.add(this)
+      }
+
+      system.addContainer(
+        "GDPR Service",
+        "NDelius GDPR service",
+        "Java"
+      ).apply {
+        uses(userManagement, "connects to", "HTTPS")
+        uses(database, "connects to", "JDBC")
+        ecs.add(this)
+      }
+
+      system.addContainer(
+        "Merge Service",
+        "NDelius Offender Merge service",
+        "Java"
+      ).apply {
+        uses(userManagement, "connects to", "HTTPS")
+        uses(database, "connects to", "JDBC")
+        ecs.add(this)
+      }
+
+      system.addContainer(
+        "Password Reset",
+        "NDelius password reset tool",
+        "Java"
+      ).apply {
+        uses(userDirectory, "updates credentials in", "LDAP")
+        ecs.add(this)
+      }
+
+      system.addContainer(
+        "OFFLOC Consumer/DSS Batch Job",
+        "Download PNOMIS data and insert into NDelius database",
+        "AWS Batch"
+      ).apply {
+        uses(ndelius, "Uses API exposed by", "HTTPS")
+        batch.add(this)
+      }
+
+      // Domain events
       probationOffenderEvents = system.addContainer(
         "probation-offender-events topic",
         "Topic receiving notifications on core offender changes",
@@ -99,27 +178,48 @@ class Delius private constructor() {
         CloudPlatform.kubernetes.add(this)
       }
 
+      // Offender search
+      offenderElasticsearchStore = system.addContainer(
+        "Offender Elasticsearch store",
+        "Data store for Delius offender data content", "Elasticsearch"
+      ).apply {
+        Tags.DATABASE.addTo(this)
+        CloudPlatform.elasticsearch.add(this)
+      }
+
       offenderSearchIndexer = system.addContainer(
         "Probation Offender Search Indexer",
-        "Service to update nDelius offender data held in Elasticsearch",
+        "Service to update NDelius offender data held in Elasticsearch",
         "Kotlin"
       ).apply {
         url = "https://github.com/ministryofjustice/probation-offender-search-indexer"
-        uses(elasticSearchStore, "Indexes offender data from nDelius to the Elasticsearch Index")
+        uses(offenderElasticsearchStore, "Indexes offender data from NDelius to the Elasticsearch Index")
         uses(probationOffenderEvents, "to know when to update a record, listens to offender changed events from", "SQS")
         uses(communityApi, "following received events, retrieves latest offender records from")
         CloudPlatform.kubernetes.add(this)
       }
 
       offenderSearch = system.addContainer(
-        "Probation Offender Search",
-        "API over the nDelius offender data held in Elasticsearch",
+        "Probation Offender Search API",
+        "API over the NDelius offender data held in Elasticsearch",
         "Kotlin"
       ).apply {
         url = "https://github.com/ministryofjustice/probation-offender-search"
-        uses(elasticSearchStore, "Queries offender data from nDelius Elasticsearch Index")
-        uses(offenderSearchIndexer, "To synchronise date from nDelius to Elasticsearch")
+        uses(offenderElasticsearchStore, "Queries offender data from NDelius Elasticsearch Index")
+        uses(offenderSearchIndexer, "To synchronise date from NDelius to Elasticsearch")
         CloudPlatform.kubernetes.add(this)
+      }
+
+      system.addContainer(
+        "Probation Offender Search Webapp / PSR (New Tech)",
+        "Search front end for NDelius offender data held in Elasticsearch / Pre-Sentence Report generation",
+        "Java/Scala"
+      ).apply {
+        url = "https://github.com/ministryofjustice/ndelius-new-tech"
+        uses(offenderSearch, "Searches offender data using")
+        uses(documentStore, "Uploads document to", "HTTPS")
+        uses(communityApi, "Accesses NDelius information using")
+        ecs.add(this)
       }
     }
 
@@ -134,26 +234,32 @@ class Delius private constructor() {
       InterventionTeams.crcProgrammeManager.interactsWith(supportTeam, "raises task to update interventions with")
       supportTeam.uses(system, "updates interventions in")
 
-      communityApi.uses(HMPPSAuth.app, "Authenticates using")
-      deliusApi.uses(HMPPSAuth.app, "Authenticates using")
+      communityApi.uses(HMPPSAuth.system, "Authenticates using")
+      deliusApi.uses(HMPPSAuth.system, "Authenticates using")
     }
 
     override fun defineViews(views: ViewSet) {
       views.createSystemContextView(system, "delius-data-context", null).apply {
         addDefaultElements()
-        model.people.forEach(this::remove)
-        enableAutomaticLayout(AutomaticLayout.RankDirection.TopBottom, 300, 300)
+        model.people.map { remove(it) }
       }
 
       views.createSystemContextView(system, "delius-people-context", null).apply {
         addDefaultElements()
-        model.softwareSystems.filter { it != system }.forEach(this::remove)
-        enableAutomaticLayout(AutomaticLayout.RankDirection.TopBottom, 300, 300)
+        model.softwareSystems.filter { it != system }.map { remove(it) }
       }
 
       views.createContainerView(system, "delius-container", null).apply {
-        addDefaultElements()
-        enableAutomaticLayout()
+        system.containers.map { add(it) }
+      }
+
+      views.createContainerView(system, "delius-container-integration-view", null).apply {
+        add(deliusApi)
+        add(communityApi)
+        add(database)
+        addNearestNeighbours(deliusApi)
+        addNearestNeighbours(communityApi)
+        remove(HMPPSAuth.system)
       }
 
       views.createDeploymentView(
@@ -162,7 +268,6 @@ class Delius private constructor() {
         "The Production deployment scenario for Delius"
       ).apply {
         add(AWS.london)
-        enableAutomaticLayout()
       }
     }
   }
