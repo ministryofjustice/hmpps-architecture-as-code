b5d1155c081143ade622d5c19280b5112577ef19
David Lantos <david.lantos@gmail.com>
2021-11-19 12:41:24 +0000

    Refactor dependency tracker into its own package
    In preparation for adding even more dependencies

---
+++ src/main/kotlin/uk/gov/justice/hmpps/architecture/documentation/versionTracker.kt
@@ -0,0 +1,49 @@
+package uk.gov.justice.hmpps.architecture.documentation
+
+import com.structurizr.model.Container
+import uk.gov.justice.hmpps.architecture.cloneRepository
+import java.io.File
+
+data class Version(
+  val softwareSystem: String,
+  val application: String,
+  val applicationUrl: String,
+  val circleciOrbVersion: String,
+  val gradleBootPluginVersion: String,
+)
+
+fun parseVersions(containersWithGit: List<Container>): List<Version> {
+  return containersWithGit
+    .also { println("[parseVersions] Found ${it.size} applications with github repos") }
+    .mapNotNull { parseVersion(it) }
+}
+
+private fun parseVersion(container: Container): Version? {
+  val r = cloneRepository(container) ?: return null
+  return Version(
+    softwareSystem = container.softwareSystem.name,
+    application = container.name,
+    applicationUrl = container.url,
+    circleciOrbVersion = readCircleOrbVersion(r),
+    gradleBootPluginVersion = readGradlePluginVersion(r)
+  )
+}
+
+private fun readCircleOrbVersion(repo: File): String {
+  val circleConfig = repo.resolve(".circleci").resolve("config.yml").takeIf { it.exists() }?.readText().orEmpty()
+
+  val circleOrb = Regex("ministryofjustice/(hmpps@[\\d.]*)").find(circleConfig)?.groups?.get(1)?.value
+  val dpsOrb = Regex("ministryofjustice/(dps@[\\d.]*)").find(circleConfig)?.groups?.get(1)?.value
+  return listOfNotNull(circleOrb, dpsOrb).joinToString("<br>")
+}
+
+const val GRADLE_PATTERN = """id\("uk.gov.justice.hmpps.gradle-spring-boot"\) version "([^"]*)""""
+private fun readGradlePluginVersion(repo: File): String {
+  val buildFileKt = repo.resolve("build.gradle.kts").takeIf { it.exists() }?.readText().orEmpty()
+  val buildFile = repo.resolve("build.gradle").takeIf { it.exists() }?.readText().orEmpty()
+
+  return listOfNotNull(
+    Regex(GRADLE_PATTERN).find(buildFileKt)?.groups?.get(1)?.value,
+    Regex(GRADLE_PATTERN).find(buildFile)?.groups?.get(1)?.value,
+  ).joinToString("<br>")
+}
