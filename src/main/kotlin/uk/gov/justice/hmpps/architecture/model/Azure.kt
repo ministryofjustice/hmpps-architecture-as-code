b5df38635bff7e627bbb44de9af1d7d48554a3f2
Stuart Whitlam <stuart.whitlam@digital.justice.gov.uk>
2022-02-11 10:16:45 +0000

    Update ARN deployments and data (#178)
    * Update ARN deployments and data

--- src/main/kotlin/uk/gov/justice/hmpps/architecture/model/Azure.kt
+++ src/main/kotlin/uk/gov/justice/hmpps/architecture/model/Azure.kt
@@ -8,11 +8,13 @@ class Azure private constructor() {
     lateinit var root: DeploymentNode
     lateinit var nomsdigitech: DeploymentNode
     lateinit var sequation: DeploymentNode
+    lateinit var kubernetes: DeploymentNode
 
     fun defineDeploymentNodes(model: Model) {
       root = model.addDeploymentNode("Microsoft Azure", "Azure platform", "Azure")
       nomsdigitech = root.addDeploymentNode("NOMS Digital Studio")
       sequation = root.addDeploymentNode("Sequation")
+      kubernetes = root.addDeploymentNode("Digital Studio AKS", "Digital Studio AKS Kubernetes Cluster", "Kubernetes")
     }
   }
 }
--- src/main/kotlin/uk/gov/justice/hmpps/architecture/model/OASys.kt
+++ src/main/kotlin/uk/gov/justice/hmpps/architecture/model/OASys.kt
@@ -6,7 +6,6 @@ import com.structurizr.model.SoftwareSystem
 import com.structurizr.view.AutomaticLayout
 import com.structurizr.view.ViewSet
 import uk.gov.justice.hmpps.architecture.HMPPSSoftwareSystem
-import uk.gov.justice.hmpps.architecture.annotations.APIDocs
 import uk.gov.justice.hmpps.architecture.annotations.Tags
 
 class OASys private constructor() {
@@ -20,7 +19,7 @@ class OASys private constructor() {
 
     override fun defineModelEntities(model: Model) {
       system = model.addSoftwareSystem("OASys", "(Offender Assessment System) Assesses the risks and needs of offenders").apply {
-        setUrl("https://dsdmoj.atlassian.net/wiki/spaces/~474366104/pages/2046820357/OASys+Overview")
+        url = "https://dsdmoj.atlassian.net/wiki/spaces/~474366104/pages/2046820357/OASys+Overview"
       }
 
       oasysDB = system.addContainer("OASys Assessments Database", null, "Oracle").apply {
@@ -31,13 +30,14 @@ class OASys private constructor() {
         Tags.DATA_API.addTo(this)
         Tags.AREA_PROBATION.addTo(this)
         uses(oasysDB, "connects to", "JDBC")
-        setUrl("https://github.com/ministryofjustice/offender-assessments-api-kotlin")
-        APIDocs("https://offender-dev.aks-dev-1.studio-hosting.service.justice.gov.uk/swagger-ui/").addTo(this)
+        url = "https://github.com/ministryofjustice/offender-assessments-api-kotlin"
+        Azure.kubernetes.add(this)
       }
 
       assessmentsEvents = system.addContainer("Offender Assessment Events", "Pushes assessment events to SQS", "Kotlin + Spring Boot").apply {
         uses(oasysDB, "connects to", "JDBC")
-        setUrl("https://github.com/ministryofjustice/offender-assessments-events")
+        url = "https://github.com/ministryofjustice/offender-assessments-events"
+        Azure.kubernetes.add(this)
       }
 
       assessmentsUpdateApi = system.addContainer(
@@ -48,13 +48,14 @@ class OASys private constructor() {
         Tags.DATA_API.addTo(this)
         Tags.AREA_PROBATION.addTo(this)
         uses(oasysDB, "connects to", "JDBC")
-        setUrl("https://github.com/ministryofjustice/offender-assessments-updates")
+        url = "https://github.com/ministryofjustice/offender-assessments-updates"
+        Azure.kubernetes.add(this)
       }
     }
 
     override fun defineRelationships() {
       ProbationPractitioners.nps.uses(system, "records offender risk (attendance, contact, etc.) and assessment in")
-      ProbationPractitioners.crc.uses(system, "records offender risk (attendance, contact, etc.) and assessment in")
+      assessmentsEvents.uses(HMPPSDomainEvents.topic, "publishes assessment domain events to", "SNS")
     }
 
     override fun defineViews(views: ViewSet) {
