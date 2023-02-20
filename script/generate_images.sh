e3fc00ffcd631d37fa1a22752a5b7d283442316f
David Lantos <david.lantos@gmail.com>
2023-02-20 13:02:03 +0000

    Update image generator versions
    PlantUML's `1.2021.16` file has moved.

--- script/generate_images.sh
+++ script/generate_images.sh
@@ -4,7 +4,7 @@ ext_dir="$root_dir/ext"
 exports_dir="$root_dir/exports"
 mkdir -p "$ext_dir"
 
-cli_version="1.17.0"
+cli_version="1.26.0"
 cli_zip="$ext_dir/cli-${cli_version}.zip"
 if [ ! -f "$cli_zip" ]; then
   echo
@@ -13,12 +13,12 @@ if [ ! -f "$cli_zip" ]; then
   unzip -o -d"$ext_dir" "$cli_zip"
 fi
 
-plantuml_version="1.2021.16.patched"
+plantuml_version="1.2023.1"
 plantuml_jar="$ext_dir/plantuml-nodot.${plantuml_version}.jar"
 if [ ! -f "$plantuml_jar" ]; then
   echo
   echo "🔧 Downloading PlantUML..."
-  wget "http://sourceforge.net/projects/plantuml/files/plantuml-nodot.${plantuml_version}.jar/download" -O"$plantuml_jar"
+  wget "http://sourceforge.net/projects/plantuml/files/${plantuml_version}/plantuml-nodot.${plantuml_version}.jar/download" -O"$plantuml_jar"
 fi
 
 echo
